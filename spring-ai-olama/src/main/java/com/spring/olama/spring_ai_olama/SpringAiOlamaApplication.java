54e74c2f9a4bdaf8e7d385e43bc177cd075ab3bd
NileshRavindraP <nileshrp239@gmail.com>
2025-12-09 23:55:22 +0530

    Spring AI Ollama FIrst project added

---
+++ spring-ai-olama/src/main/java/com/spring/olama/spring_ai_olama/SpringAiOlamaApplication.java
@@ -0,0 +1,13 @@
+package com.spring.olama.spring_ai_olama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiOlamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiOlamaApplication.class, args);
+	}
+
+}
---
+++ spring-ai-olama/src/test/java/com/spring/olama/spring_ai_olama/SpringAiOlamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.olama.spring_ai_olama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiOlamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
